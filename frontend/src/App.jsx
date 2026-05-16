4f7d78ad22c46999a4263c17ab00e1d35ef8f1b1
Tejash Sharma <sharmatejash868@gmail.com>
2026-05-16 16:50:07 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,8 +56,7 @@ const App = () => {
     localStorage.setItem("userInfo", JSON.stringify(userInfo));
     navigate("/", { replace: true });
   };
-
-  const handleLogout = () => {
+const handleLogout = () => {
   // Remove user data
   localStorage.removeItem("userInfo");
 
